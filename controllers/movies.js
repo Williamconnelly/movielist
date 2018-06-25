4271391a028ae3ecf4a918c5b5838c52fe26b2a3
William Connelly <williamconnelly.dev@gmail.com>
2018-06-25 01:04:01 -0700

    Added search display functionality

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,23 +5,22 @@ var request = require("request")
 var router = express.Router();
 require('dotenv').config();
 
-router.get("/", function(req, res) {
-	res.render("movies/index");
-});
+// router.get("/", function(req, res) {
+// 	res.render("movies/index");
+// });
 
 router.post("/", function(req, res) {
-	console.log(req.body.movie_search);
 	request({
-		url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB + "&t=" + req.body.movie_search
+		url: "http://www.omdbapi.com/?apikey=" + process.env.OMDB + "&s=" + req.body.movie_search
 	}, function(error, response, body) {
 		if (!error && response.statusCode === 200) {
-			var dataObj = JSON.parse(body);
-			console.log(dataObj);
+			var movies = JSON.parse(body);
+			console.log(movies.Search);
+			res.render("movies/index", {movies: movies.Search});
 		} else {
 			console.log(error, response);
 		};
 	});
-	res.redirect("/movies");
 });
 
 module.exports = router;
